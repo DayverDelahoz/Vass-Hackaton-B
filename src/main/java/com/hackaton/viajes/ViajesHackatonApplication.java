9987696f303b167828c44650a1a58a099d141c1c
VASSLATAM1\dayver.delahoz <dayver.delahoz@vasslatam.com>
2022-11-25 20:22:45 -0500

    Primer commit

---
+++ src/main/java/com/hackaton/viajes/ViajesHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesHackatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesHackatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/viajes/ViajesHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesHackatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
